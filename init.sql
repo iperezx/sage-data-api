6ba06f37c46deceb500d751973b07c00ebf41ba5
iperezx <i3perez@sdsc.edu>
2022-10-31 15:00:15 -0700

    Remove sql artifacts.

--- init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS SageStorage.Nodes (
-    nodeid          VARCHAR(64) NOT NULL,
-    metadata_name   VARCHAR(64),
-    metadata_value  VARCHAR(64)
-);
-
-INSERT INTO SageStorage.Nodes (nodeid, metadata_name, metadata_value) VALUES 
-( '4cd98fc4d2a8' ,'name','Sage-NEON-01');
-
